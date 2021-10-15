d525d0b48ad14f50067860fe554347d7caa79cf7
Navya Rajput <navyasrajput@gmail.com>
2021-10-15 00:23:20 -0400

    Update LighthouseBnB Properties for searching

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -122,17 +122,86 @@ exports.getAllReservations = getAllReservations;
 */
 
 const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      //console.log(result.rows);
-      return result.rows;
+  //return pool
+  //.query(`SELECT * FROM properties LIMIT $1`, [limit])
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, AVG (property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
 
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+
+    queryString += `
+    WHERE city LIKE $${queryParams.length}`;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+
+    if (options.city) {
+      queryString += `
+      AND properties.owner_id = $${queryParams.length}`;
+    } else {
+      queryString += `
+      WHERE properties.owner_id = $${queryParams.length}`;
+    }
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night);
+
+    if (options.city || options.owner_id) {
+      queryString += `
+      AND properties.cost_per_night >= $${queryParams.length}`;
+    } else {
+      queryString += `
+      WHERE properties.cost_per_night >= $${queryParams.length}`;
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night);
+
+    if (options.city || options.owner_id || options.minimum_price_per_night) {
+      queryString += `
+      AND properties.cost_per_night <= $${queryParams.length}`;
+    } else {
+      queryString += `
+      WHERE properties.cost_per_night <= $${queryParams.length}`;
+    }
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating, limit);
+
+    queryString += `
+    GROUP BY properties.id
+    HAVING AVG (property_reviews.rating) >= $${queryParams.length - 1}
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  } else {
+    queryParams.push(limit);
+
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  }
+
+  return pool.query(queryString, queryParams)
+
+    .then(res => res.rows)
+  //console.log(result.rows);
+  //return result.rows;
+
+}
+//.catch((err) => {
+// console.log(err.message);
 exports.getAllProperties = getAllProperties;
 
 
